34a9441a0fc8a936be262461bcc29a56ff6f859f
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-02 17:02:33 -0300

    [feat] Adiciona exibição de texto no console

--- C-Sharp-Sandbox/Program.cs
+++ C-Sharp-Sandbox/Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿// Módulos/pacotes que serão utilizados
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 
 namespace C_Sharp_Sandbox
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Função principal -> Primeiro bloco de código que será executado
         {
+            Console.Write("Este comando escreve o texto entre aspas no prompt de comando");
 
+            Console.Write("\n\nAcrescentando quebra de linha\ndentro do texto\n\n");
+            Console.Write("Se não quebrar linha... ");
+            Console.Write("O texto será escrito um ao lado do outro.\n\n");
+
+            Console.WriteLine("Este comando acrescenta uma quebra de linha depois do texto");
+            Console.Write("Hello World!");
         }
     }
 }
