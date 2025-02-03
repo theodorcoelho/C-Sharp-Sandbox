ad207b41213852210f86e0b1b03f6e45f493cc05
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-03 02:37:51 -0300

    [feat] Adiciona declaração de arrays

---
+++ C-Sharp-Sandbox/Arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp_Sandbox
+{
+    internal class Arrays
+    {
+        static void Main(string[] args)
+        {
+            // Declarando arrays
+            string[] produtos = new string[5]  { // Não é possível modificar o tamanho do array após sua declaração
+           
+                "Produto 1",
+                "Produto 2",
+                "Produto 3",
+                "Produto 4",
+                "Produto 5"
+            };
+
+            int[] valores = new int[5] { 3, 5, 7, 9, 11 }; // Inicializa o array com a quantidade de itens que foi declarado
+
+            Console.WriteLine(produtos[1]); // Escreve o produto que está alocado no índice 1 (2 posição)
+            Console.WriteLine(valores[4]); // Escreve o valor que está alocado no índice 2 (5 posição)
+        }
+    }
+}
