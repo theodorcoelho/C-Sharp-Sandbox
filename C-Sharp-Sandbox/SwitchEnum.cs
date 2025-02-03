aff2ef342e9b87faaf5cdc2dab1ac63e1a151ce6
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-03 03:10:17 -0300

    [feat] Adiciona switch case

--- C-Sharp-Sandbox/SwitchEnum.cs
+++ C-Sharp-Sandbox/SwitchEnum.cs
@@ -9,7 +9,26 @@ namespace C_Sharp_Sandbox
     {
         static void Main(string[] args)
         {
+            string corEscolhida = "Azul";
 
+            switch(corEscolhida)
+            {
+                case "Vermelho":
+                    Console.WriteLine("Você escolheu vermelho");
+                    break;
+                case "Azul":
+                    Console.WriteLine("Você escolheu azul");
+                    break;
+                case "Roxo":
+                    Console.WriteLine("Você escolheu roxo");
+                    break;
+                case "Verde":
+                    Console.WriteLine("Você escolheu verde");
+                    break;
+                default: // opção padrão que será executada se não entrar em nenhum case
+                    Console.WriteLine("Ainda não trabalhamos com esta cor.");
+                    break;
+            }
         }
     }
 }
