98caa5841e8ae978e199736ae0fa5fc347d033aa
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-04 09:29:15 -0300

    [feat] Adiciona foreach

--- C-Sharp-Sandbox/Loops.cs
+++ C-Sharp-Sandbox/Loops.cs
@@ -9,6 +9,7 @@ namespace C_Sharp_Sandbox
     {
         static void Main(string[] args)
         {
+            // WHILE LOOP
             int index = 0;
             while (index < 5) // enquanto a condição for verdadeira, o loop será executado
             {
@@ -19,11 +20,29 @@ namespace C_Sharp_Sandbox
             Console.WriteLine("\nWhile Loop executado " + index + " vezes.\n");
             index = 0; // zerando o contador
 
+            // DO WHILE LOOP
+
             do // programa será executado uma vez e se a condição no final for verdadeira, será executado novamente
             {
                 Console.WriteLine("Estrutura de repetição do while!");
                 index++;
             } while (index < 5);
+            Console.Write("\n");
+            // FOREACH
+
+            string[] nomes = { "Theodor", "Pedro", "Lupin", "Lucas", "Jose"};
+            Console.Write("Insira o nome que está buscando: ");
+            string nomeBuscado = Console.ReadLine();
+
+            foreach (string nome in nomes) // Para cada nome do array nomes, repita o bloco de código
+            {
+                Console.Write(nome);
+                if (nome == nomeBuscado)
+                {
+                    Console.Write("  <--- Encontrei!");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
