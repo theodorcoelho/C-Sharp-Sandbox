1f6190efcc39c444d44fec5bb26ff9e7b913bbc9
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-04 08:57:03 -0300

    [chore] Adiciona arquivo Loops

---
+++ C-Sharp-Sandbox/Loops.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp_Sandbox
+{
+    internal class Loops
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
